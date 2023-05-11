d791ca24e32c694d60e4cc2953f29dbb2a816c53
aslavinska <anastasija.slavinska@avl.com>
2023-05-11 12:48:01 +0000

    Completed user stories manage posts, create drafts, approve comments

--- blog/admin.py
+++ blog/admin.py
@@ -11,8 +11,7 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
-
-
+    
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
