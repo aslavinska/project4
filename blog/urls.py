0186b784d0cc69132f41bdbd0e5cc585ff6664fa
aslavinska <anastasija.slavinska@avl.com>
2023-05-29 10:23:59 +0000

    Adding css js template for the website UI

--- blog/urls.py
+++ blog/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from .views import contact, success
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='blog'),
+    path('blog/', views.PostList.as_view(), name='blog'),
 
     path('contact/', contact, name='contact'),
     path('success/', success, name='success'),
     path('about/', views.about, name='about'),
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
 
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
