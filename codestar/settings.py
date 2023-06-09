741114cfac98726b79bee5c00c9e886d35bf24fa
aslavinska <anastasija.slavinska@avl.com>
2023-06-09 08:53:11 +0000

    fixing styling html pages and adding new images

--- codestar/settings.py
+++ codestar/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost", "8000-aslavinska-project4-7uamhidnrbh.ws-eu98.gitpod.io"]
+ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost", "8000-aslavinska-project4-7uamhidnrbh.ws-eu99.gitpod.io"]
 
 
 
