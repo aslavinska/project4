586c328aa51e12d6e05c95794a75a261a9024966
aslavinska <anastasija.slavinska@avl.com>
2023-06-28 13:56:38 +0000

    Deployment Commit

--- sailorshushu/settings.py
+++ sailorshushu/settings.py
@@ -19,9 +19,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-#X_FRAME_OPTIONS = 'SAMEORIGIN'
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ["https://aslavinska-project4.herokuapp.com/", "aslavinska-project4.herokuapp.com", "localhost", "8000-aslavinska-project4-7uamhidnrbh.ws-eu101.gitpod.io"]
 
